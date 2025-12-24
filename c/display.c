a838cf681fc19fff231ffbe257dc997e3730b40c
CherryFloors <d.ryan.barden@gmail.com>
2025-12-24 13:35:07 -0500

    standy screen

--- c/display.c
+++ c/display.c
@@ -3,6 +3,8 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_keyboard.h>
 #include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_ttf.h>
@@ -14,6 +16,57 @@
 SDL_Window *window;
 SDL_Texture *text_texture;
 TTF_Font *font;
+
+typedef struct VcrColorPallete {
+    SDL_Color c0;
+    SDL_Color c1;
+    SDL_Color c2;
+    SDL_Color c3;
+    SDL_Color c4;
+    SDL_Color text_fg;
+    SDL_Color text_bg;
+} VcrColorPallete;
+
+VcrColorPallete default_color_pallete(void) {
+    VcrColorPallete vcr_color_pallete = {
+        {0xFD, 0xC3, 0x31}, {0xFE, 0x57, 0x22}, {0xF0, 0x35, 0x3C}, {0xB4, 0x21, 0x3D},
+        {0x67, 0x1C, 0x3B}, {0xFF, 0xFF, 0xFF}, {0x1A, 0x1A, 0x1A},
+    };
+    return vcr_color_pallete;
+}
+
+void standby_screen(SDL_Renderer *renderer) {
+    VcrColorPallete colors = default_color_pallete();
+    SDL_SetRenderDrawColor(renderer, colors.text_fg.r, colors.text_fg.g, colors.text_fg.b, 255);
+    SDL_RenderClear(renderer);
+
+    int padding = 125;
+    int bar_height = (480 - padding * 2) / 5;
+
+    SDL_Rect r0 = {0, padding + (bar_height * 0), 640, bar_height};
+    SDL_Rect r1 = {0, padding + (bar_height * 1), 640, bar_height};
+    SDL_Rect r2 = {0, padding + (bar_height * 2), 640, bar_height};
+    SDL_Rect r3 = {0, padding + (bar_height * 3), 640, bar_height};
+    SDL_Rect r4 = {0, padding + (bar_height * 4), 640, bar_height};
+
+    SDL_SetRenderDrawColor(renderer, colors.c0.r, colors.c0.g, colors.c0.b, 255);
+    SDL_RenderFillRect(renderer, &r0);
+
+    SDL_SetRenderDrawColor(renderer, colors.c1.r, colors.c1.g, colors.c1.b, 255);
+    SDL_RenderFillRect(renderer, &r1);
+
+    SDL_SetRenderDrawColor(renderer, colors.c2.r, colors.c2.g, colors.c2.b, 255);
+    SDL_RenderFillRect(renderer, &r2);
+
+    SDL_SetRenderDrawColor(renderer, colors.c3.r, colors.c3.g, colors.c3.b, 255);
+    SDL_RenderFillRect(renderer, &r3);
+
+    SDL_SetRenderDrawColor(renderer, colors.c4.r, colors.c4.g, colors.c4.b, 255);
+    SDL_RenderFillRect(renderer, &r4);
+
+    SDL_RenderPresent(renderer);
+    SDL_RenderClear(renderer);
+}
 
 bool process_key_stroke(SDL_Keysym symbol) {
     bool should_quit = false;
@@ -69,6 +122,11 @@ bool handle_event(SDL_Event *event, SDL_Renderer *renderer) {
         SDL_RenderPresent(renderer);
         should_quit = process_key_stroke(event->key.keysym);
         SDL_RenderClear(renderer);
+
+        if (event->key.keysym.sym == SDLK_s) {
+            standby_screen(renderer);
+        }
+
         break;
     }
 
@@ -106,6 +164,7 @@ int run_display_loop(void) {
     window = SDL_CreateWindow("vcr display", window_x_pos, window_y_pos, 640, 480, SDL_WINDOW_BORDERLESS);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 
+    standby_screen(renderer);
     while (true) {
         SDL_Event Event;
         SDL_WaitEvent(&Event);
